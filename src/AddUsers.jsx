db51a5d4fd42e706febe388ab93133c8c5847254
DIALLO <amadou9393@yahoo.fr>
2021-07-22 03:21:56 +0000

    putingt in separated components

---
+++ src/AddUsers.jsx
@@ -0,0 +1,62 @@
+import react from "react";
+import React, { Component } from 'react';
+class AddUsers extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            name:"",
+            phone:"",
+            email:"",
+            password:""
+        }
+    }
+    managechange=(e)=>{
+        e.preventDefault()
+        this.setState({[e.target.id] : e.target.value});
+        
+    }
+    managesubmit=(e)=>{
+        e.preventDefault()
+       this.props.newUser(this.state)
+       this.setState({
+        name:"",
+        phone:"",
+        email:"",
+        password:"" 
+       })
+    }
+
+    render() {
+        return (
+            <div>
+               
+                <form style={{marginTop:"2rem",marginLeft:"10rem"}} onSubmit={this.managesubmit} >
+                    <h1>Add Users</h1>
+                    <div>
+                        <label>Name : </label>
+                        <input style={{marginBottom:"5px"}} type="text" id="name" value={this.state.name} onChange={this.managechange} placeholder="your name please" required/>
+                    </div>
+                    <div>
+                        <label>phone : </label>
+                        <input style={{marginBottom:"5px"}} type="number" id="phone" value={this.state.phone} onChange={this.managechange}  placeholder="your name please" required/>
+                    </div>
+                    <div>
+                        <label>Email : </label>
+                        <input style={{marginBottom:"5px"}}type="Email" id="email" value={this.state.email} onChange={this.managechange} placeholder="your name please" required/>
+                    </div>
+                    
+                    <div>
+                        <label>password : </label>
+                        <input style={{marginBottom:"5px"}} type="password" id="password" value={this.state.password} onChange={this.managechange} placeholder="your name please" required/>
+                    </div>
+                    <button>Submit</button>
+                </form> 
+            </div>
+        );
+    }
+}
+
+export default AddUsers;
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
-import Users from "./Users";
-function App() {
-  return (
-    <Users/>
-  );
+import React, { Component } from 'react';
+import AddUsers from './AddUsers';
+import ListUsers from './ListUsers';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+  addinfo = (user) => {
+    this.setState({ users: [...this.state.users, user] 
+    })
+  }
+  render() {
+    return (
+      <div>
+        <AddUsers newUser={this.addinfo} />
+        {this.state.users.map((list, index) => {
+          return (
+            <ListUsers user={list} />
+          )
+        })}
+
+      </div>
+    );
+  }
 }
 
 export default App;
